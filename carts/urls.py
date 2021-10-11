14a5e0420635db6a6e4fe3d9d17223abd5e33784
Ali ZORLU <azorlu80@gmail.com>
2021-10-11 13:01:39 +0300

    second commit

--- carts/urls.py
+++ carts/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.cart, name='cart')
+    path('', views.cart, name='cart'),
+    path('/add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('/decrease_cart_item/<int:product_id>/', views.decrease_cart_item, name='decrease_cart_item'),
+    path('/remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
+
 ]
